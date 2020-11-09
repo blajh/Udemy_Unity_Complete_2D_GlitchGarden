632d7ab11e2f950afe0faed08e068aade8e3ed84
Andrej <glazba@gmail.com>
2020-11-09 20:39:49 +0100

    loader

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,13 +6,25 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private float _splashLoadDelay = 3f;
+	private int _currentSceneIndex;
 
-	private void Awake() {
-		StartCoroutine(LoadStartScene());
+	private void Start () {
+		CheckIfSplashScreen();
 	}
 
-	IEnumerator LoadStartScene() {
+	private void CheckIfSplashScreen () {
+		_currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		if (_currentSceneIndex == 0) {
+			StartCoroutine(WaitForTime());
+		}
+	}
+
+	IEnumerator WaitForTime () {
 		yield return new WaitForSeconds(_splashLoadDelay);
-		SceneManager.LoadScene(1);
+		LoadNextScene();
+	}
+
+	private void LoadNextScene () {
+		SceneManager.LoadScene(_currentSceneIndex + 1);
 	}
 }
