be5443a53b75fcfcd00ea917a82c1ae15afeb4e4
Andrej <glazba@gmail.com>
2020-11-09 20:32:57 +0100

    Load Star From Splash with Delay

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float _splashLoadDelay = 3f;
+
+	private void Awake() {
+		StartCoroutine(LoadStartScene());
+	}
+
+	IEnumerator LoadStartScene() {
+		yield return new WaitForSeconds(_splashLoadDelay);
+		SceneManager.LoadScene(1);
+	}
+}
