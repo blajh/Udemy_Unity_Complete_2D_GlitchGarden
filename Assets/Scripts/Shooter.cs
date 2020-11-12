dcb60da6504176ea1a2ce44ab644c2a594775dd4
Andrej <glazba@gmail.com>
2020-11-13 00:01:46 +0100

    Instantiate from SpawnPos

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+	[SerializeField] private GameObject _projectile;
+	[SerializeField] private GameObject _spawnPoint;
+
+	public void Fire() {
+		Instantiate(_projectile, _spawnPoint.transform.position, Quaternion.identity);
+	}
+}
